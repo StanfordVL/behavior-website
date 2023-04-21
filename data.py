806949cf9650f5feba2373312de176603e6f12a2
Wensi Ai <vinceai@outlook.com>
2023-04-20 19:53:23 -0700

    Remove build folder; Update integration to distinguish current/future

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 import os
 import csv
 import glob 
+import gspread
 import json
 import pathlib
-import yaml
 import networkx as nx
 
 from bddl.activity import Conditions
@@ -13,7 +13,7 @@ from utils import *
 
 
 
-def get_all_tasks():
+def get_tasks():
     """
     Get a list of all defined tasks
     returns:
@@ -37,37 +37,39 @@ def get_scenes():
     """
     Get a list of defined and available scenes (and it's containing scene-objects)
     returns:
-        scene_to_objects: dict mapping scene name to the object list that contains the scene definition
-        available_scenes: list of available scenes
+        all_scenes: dict mapping all planned scenes to their room and object configurations
+        cur_scenes: dict mapping currently available scenes to their room and object configurations
     """
-    scene_to_objects = {}
-    with open(rf"{os.path.pardir}/ig_pipeline/metadata/combined_room_object_list.json", "r") as f:
-        scene_to_objects = json.load(f)["scenes"]
-    with open("params.yaml", "r") as f:
-        available_scenes = yaml.safe_load(f)["final_scenes"]
-    available_scenes = [x.split('/')[-1] for x in available_scenes] # remove 'scene/' prefix
-    return scene_to_objects, available_scenes
+    with open(rf"{os.path.pardir}/ig_pipeline/artifacts/pipeline/combined_room_object_list_future.json", "r") as f:
+        all_scenes = json.load(f)["scenes"]
+    with open(rf"{os.path.pardir}/ig_pipeline/artifacts/pipeline/combined_room_object_list.json", "r") as f:
+        cur_scenes = json.load(f)["scenes"]
+    return all_scenes, cur_scenes
 
 
-def get_objects_and_categories(scenes):
+def get_objects_and_categories():
     """
     Get objects and categories and their correspondence
     returns:
-        all_objects: list of all objects (planned) in the dataset
+        all_objects: list of all planned objects in the dataset
+        cur_objects: list of currently available objects in the dataset
         cat_to_object: dict mapping category to list of available objects in that category
         provided_categories: set of categories that are provided by the dataset
         available_objects: list of available objects in the dataset
     """
-    cat_to_object = defaultdict(list)
+    all_cat_to_object, cur_cat_to_object = defaultdict(list), defaultdict(list)
+    with open(f"{os.path.pardir}/ig_pipeline/artifacts/pipeline/object_inventory_future.json", "r") as f:
+        all_objects = sorted(json.load(f)["providers"].keys())
     with open(f"{os.path.pardir}/ig_pipeline/artifacts/pipeline/object_inventory.json", "r") as f:
-        obj_json = json.load(f)
-    available_objects = sorted(obj_json["providers"].keys())
-    all_objects = sorted(obj_json["all"].keys())
+        cur_objects = sorted(json.load(f)["providers"].keys())
     for obj in all_objects:
-        cat_to_object[obj.split("-")[0]].append(obj)
-    provided_categories = {x.split("-")[0] for x in all_objects}
+        all_cat_to_object[obj.split("-")[0]].append(obj)
+    for obj in cur_objects:
+        cur_cat_to_object[obj.split("-")[0]].append(obj)
+    all_categories = {x.split("-")[0] for x in all_objects}
+    cur_categories = {x.split("-")[0] for x in all_objects}
 
-    return all_objects, cat_to_object, provided_categories, available_objects
+    return all_objects, cur_objects, all_cat_to_object, cur_cat_to_object, all_categories, cur_categories
 
 
 def get_all_synsets():
@@ -133,17 +135,24 @@ def get_task_synset_mapping(G, task_to_fn):
     return task_to_synset, synset_to_task, task_to_scene_synset, task_to_non_scene_synset, task_to_legal_synsets, task_to_illegal_synsets, task_requirements
 
 
-def get_category_synset_mapping(provided_categories):
+def get_category_synset_mapping(all_categories, cur_categories):
     """
     Get the mapping between object catorgory and synset
     One synset can correspond to multiple categories
     returns:
-        cat_to_synset: dict mapping category to corresponding synset
-        synset_to_cat: dict mapping synset to corresponding category
+        all_cat_to_synset: dict mapping category to corresponding synset
+        cur_cat_to_synset: dict mapping category to corresponding synset
+        all_synset_to_cat: dict mapping synset to corresponding category
+        cur_synset_to_cat: dict mapping synset to corresponding category
     """
     # Get the category - synset mapping
-    cat_to_synset = {}
-    synset_to_cat = defaultdict(list)
+    all_cat_to_synset, cur_cat_to_synset = {}, {}
+    all_synset_to_cat, cur_synset_to_cat = defaultdict(list), defaultdict(list)
+    gc = gspread.service_account(filename=os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+    worksheet = gc.open_by_key("10L8wjNDvr1XYMMHas4IYYP9ZK7TfQHu--Kzoi0qhAe4").worksheet("Object Category Mapping")
+    with open(f"{os.path.pardir}/category_mapping.csv", 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(worksheet.get_all_values())
     with open(f"{os.path.pardir}/category_mapping.csv", newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -152,53 +161,70 @@ def get_category_synset_mapping(provided_categories):
             if not synset or not category:
                 print(f"Skipping problematic row: {row}")
                 continue
-            if category not in provided_categories:
-                continue
             synset = canonicalize(synset)
-            cat_to_synset[category] = synset
-            synset_to_cat[synset].append(category)
-    return cat_to_synset, synset_to_cat
+            if category in cur_categories:
+                cur_cat_to_synset[category] = synset
+                cur_synset_to_cat[synset].append(category)
+            if category in all_categories:
+                all_cat_to_synset[category] = synset
+                all_synset_to_cat[synset].append(category)
+    return all_cat_to_synset, cur_cat_to_synset, all_synset_to_cat, cur_synset_to_cat
 
 
-def get_available_synsets(G, synset_to_cat, task_to_legal_synsets, cat_to_object):
+def get_available_synsets(G, task_to_legal_synsets, all_synset_to_cat, cur_synset_to_cat, all_cat_to_object, cur_cat_to_object):
     """
     Analyze whether task required synsets are available in the dataset
     returns:
-        synset_to_objects: dict mapping synset to the objects that are available in the dataset
-        task_to_found_synset: dict mapping task name to the synsets that has available objects in the dataset
-        task_to_not_found_synset: dict mapping task name to the synsets that has no available objects in the dataset
+        all_synset_to_objects: dict mapping synset to all planned objects
+        cur_synset_to_objects: dict mapping synset to currently available objects
+        all_task_to_found_synset: dict mapping task name to the synsets that has available objects in the future planned dataset
+        cur_task_to_found_synset: dict mapping task name to the synsets that has available objects in the currently available dataset
+        all_task_to_not_found_synset: dict mapping task name to the synsets that has no available objects in the future planned dataset
+        cur_task_to_not_found_synset: dict mapping task name to the synsets that has no available objects in the currently available dataset
     """
-    found_synsets = set(synset_to_cat.keys())
-    synset_to_objects = defaultdict(set)
-    for s in {ss for value in task_to_legal_synsets.values() for ss in value}:
-        # Get the tree of the synset
-        child_synsets = [s] + list(nx.descendants(G, s))
-        for cs in child_synsets:
-            if cs in found_synsets:
-                s_cats = synset_to_cat[cs]
-                s_objs = {obj for cat in s_cats for obj in cat_to_object[cat]}
-                synset_to_objects[s].update(s_objs)
-        if s in synset_to_objects:
-            synset_to_objects[s] = sorted(synset_to_objects[s])
+    all_synset_to_objects, cur_synset_to_objects = defaultdict(set), defaultdict(set)
+    all_task_to_found_synset, cur_task_to_found_synset = {}, {}
+    all_task_to_not_found_synset, cur_task_to_not_found_synset = {}, {}
+    for synset_to_cat, cat_to_object, synset_to_objects, task_to_found_synset, task_to_not_found_synset in zip(
+        [all_synset_to_cat, cur_synset_to_cat], 
+        [all_cat_to_object, cur_cat_to_object], 
+        [all_synset_to_objects, cur_synset_to_objects], 
+        [all_task_to_found_synset, cur_task_to_found_synset], 
+        [all_task_to_not_found_synset, cur_task_to_not_found_synset]
+    ):
+        found_synsets = set(synset_to_cat.keys())
+        for s in {ss for value in task_to_legal_synsets.values() for ss in value}:
+            # Get the tree of the synset
+            child_synsets = [s] + list(nx.descendants(G, s))
+            for cs in child_synsets:
+                if cs in found_synsets:
+                    s_cats = synset_to_cat[cs]
+                    s_objs = {obj for cat in s_cats for obj in cat_to_object[cat]}
+                    synset_to_objects[s].update(s_objs)
+            if s in synset_to_objects:
+                synset_to_objects[s] = sorted(synset_to_objects[s])
+        for task_name in task_to_legal_synsets:
+            task_to_not_found_synset[task_name] = sorted(set(task_to_legal_synsets[task_name]) - set(synset_to_objects.keys()))
+            task_to_found_synset[task_name] = sorted(set(task_to_legal_synsets[task_name]) - set(task_to_not_found_synset[task_name]))
+        
+    return all_synset_to_objects, cur_synset_to_objects, all_task_to_found_synset, cur_task_to_found_synset, all_task_to_not_found_synset, cur_task_to_not_found_synset
 
-    task_to_not_found_synset = {task_name: sorted(set(task_to_legal_synsets[task_name]) - set(synset_to_objects.keys())) for task_name in task_to_legal_synsets}
-    task_to_found_synset = {task_name: sorted(set(task_to_legal_synsets[task_name]) - set(task_to_not_found_synset[task_name])) for task_name in task_to_legal_synsets}
-    return synset_to_objects, task_to_found_synset, task_to_not_found_synset
 
-
-def get_task_scene_mapping(G, task_to_fn, scene_to_objects, task_requirements):
+def get_task_scene_mapping(G, task_to_fn, all_scenes, cur_scenes, task_requirements):
     """
-    For each task, find all scenes that compatible with it
+    For each task, find scenes that compatible with it
     returns:
-        task_to_scene: dict mapping task name to whether the scenes are compatible with it
+        all_task_to_scene: dict mapping task name to whether the planned scenes are compatible with it
+        cur_task_to_scene: dict mapping task name to whether the currently available scenes are compatible with it
     """
-    task_to_scene = {}
-    for task_name in task_to_fn:
-        task_to_scene[task_name] = {"matched": [], "unmatched": {}}
-        for scene in scene_to_objects:
-            ret = scene_compatible_with_task(G, scene_to_objects, scene, task_requirements[task_name]) 
-            if len(ret) == 0:
-                task_to_scene[task_name]["matched"].append(scene)
-            else:
-                task_to_scene[task_name]["unmatched"][scene] = ret
-    return task_to_scene
+    all_task_to_scene, cur_task_to_scene = {}, {}
+    for task_to_scene, scenes in zip([all_task_to_scene, cur_task_to_scene], [all_scenes, cur_scenes]):
+        for task_name in task_to_fn:
+            task_to_scene[task_name] = {"matched": [], "unmatched": {}}
+            for scene in scenes:
+                ret = scene_compatible_with_task(G, scenes, scene, task_requirements[task_name]) 
+                if len(ret) == 0:
+                    task_to_scene[task_name]["matched"].append(scene)
+                else:
+                    task_to_scene[task_name]["unmatched"][scene] = ret
+    return all_task_to_scene, cur_task_to_scene
--- server.py
+++ server.py
@@ -1,39 +1,62 @@
+import argparse
 from flask import Flask, render_template, abort
 from data import *
 from flask_frozen import Freezer
 
 
 G = get_all_synsets()
-scene_to_objects, available_scenes = get_scenes()
-object_to_fn, cat_to_object, provided_categories, available_objects = get_objects_and_categories(scene_to_objects)
-cat_to_synset, synset_to_cat = get_category_synset_mapping(provided_categories)
-tasks_to_fn = get_all_tasks()
+tasks_to_fn = get_tasks()
+all_scenes, cur_scenes = get_scenes()
+all_objects, cur_objects, all_cat_to_object, cur_cat_to_object, all_categories, cur_categories = get_objects_and_categories()
+all_cat_to_synset, cur_cat_to_synset, all_synset_to_cat, cur_synset_to_cat = get_category_synset_mapping(all_categories, cur_categories)
 task_to_synset, synset_to_task, task_to_scene_synset, task_to_non_scene_synset, task_to_legal_synsets, task_to_illegal_synsets, task_requirements = get_task_synset_mapping(G, tasks_to_fn)
-task_to_scene = get_task_scene_mapping(G, tasks_to_fn, scene_to_objects, task_requirements)
-synset_to_objects, task_to_found_synset, task_to_not_found_synset = get_available_synsets(G, synset_to_cat, task_to_legal_synsets, cat_to_object)
+all_task_to_scene, cur_task_to_scene = get_task_scene_mapping(G, tasks_to_fn, all_scenes, cur_scenes, task_requirements)
+all_synset_to_objects, cur_synset_to_objects, all_task_to_found_synset, cur_task_to_found_synset, all_task_to_not_found_synset, cur_task_to_not_found_synset = \
+    get_available_synsets(G, task_to_legal_synsets, all_synset_to_cat, cur_synset_to_cat, all_cat_to_object, cur_cat_to_object)
+
+# construct task status
 tasks_status = {
-    task_name: [task_to_illegal_synsets[task_name], task_to_scene[task_name], task_to_found_synset[task_name], task_to_not_found_synset[task_name]] for task_name in tasks_to_fn
+    task_name: [
+        task_to_illegal_synsets[task_name], 
+        cur_task_to_scene[task_name], 
+        cur_task_to_found_synset[task_name],
+        cur_task_to_not_found_synset[task_name],
+        all_task_to_scene[task_name], 
+        all_task_to_found_synset[task_name],
+        all_task_to_not_found_synset[task_name],
+    ] for task_name in tasks_to_fn
 }
 
 # metadata
-success_task = len([x for x in tasks_status if len(tasks_status[x][0]) == 0 and len(tasks_status[x][1]) > 0 and len(tasks_status[x][3]) == 0])
-success_synset = len([x for x in synset_to_cat if len(synset_to_objects[x]) == 1])
-metadata = [[success_task, len(tasks_to_fn)], [success_synset, len(synset_to_cat)], [len(available_objects), len(object_to_fn)], [len(available_scenes), len(scene_to_objects)]]
+n_success_task = len([x for x in tasks_status if len(tasks_status[x][0]) == 0 and len(tasks_status[x][1]) > 0 and len(tasks_status[x][3]) == 0])
+n_pending_task = len([x for x in tasks_status if len(tasks_status[x][0]) == 0 and len(tasks_status[x][4]) > 0 and len(tasks_status[x][6]) == 0])
+n_success_synset = len([x for x in all_synset_to_cat if len(cur_synset_to_objects[x]) > 0])
+n_pending_synset = len([x for x in all_synset_to_cat if len(all_synset_to_objects[x]) > 0])
+
+metadata = [
+    [n_success_task, n_pending_task, len(tasks_to_fn)],             # tasks
+    [n_success_synset, n_pending_synset, len(all_synset_to_cat)],   # synsets
+    [len(cur_objects), len(all_objects)],                           # objects 
+    [len(cur_scenes), len(all_scenes)]                              # scenes
+]
 
 
 # ==================================================================================================
+parser = argparse.ArgumentParser(description='Generate static website for the dataset')
+parser.add_argument('-d', '--debug', action='store_true', help='debug mode (build local server)')
+args = parser.parse_args()
+prefix = "" if args.debug else "/b1k-integration"
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('index.html', metadata=metadata)
+    return render_template('index.html', metadata=metadata, prefix=prefix)
 
 
 @app.route('/tasks/')
 def tasks_list():
-    return render_template('tasks_list.html', tasks_status=tasks_status)
+    return render_template('tasks_list.html', tasks_status=tasks_status, prefix=prefix)
 
 
 @app.route('/tasks/<task_name>.html')
@@ -44,9 +67,9 @@ def tasks(task_name):
     return render_template('task.html', task_name=task_name, task_data=task_data)
 
 
-@app.route('/objects.html')
+@app.route('/synsets.html')
 def objects():
-    return render_template('objects.html', synset_to_task=synset_to_task, synset_to_objects=synset_to_objects)
+    return render_template('synsets.html', synset_to_task=synset_to_task, all_synset_to_objects=all_synset_to_objects, cur_synset_to_objects=cur_synset_to_objects)
 
 
 @app.template_filter('format_iterable')
@@ -55,6 +78,10 @@ def format_list(iter):
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    freezer = Freezer(app)
-    freezer.freeze()
+    if args.debug:
+        print("Building local webserver...")
+        app.run()
+    else:
+        print("Generating static website files...")
+        freezer = Freezer(app)
+        freezer.freeze()
