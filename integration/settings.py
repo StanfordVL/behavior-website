8e90158f7a3b05c17200a169e459c7ea6f6e91d9
Cem Gökmen <cgokmen@stanford.edu>
2023-05-24 05:43:40 -0700

    Back to SQLite

--- integration/settings.py
+++ integration/settings.py
@@ -79,17 +79,9 @@ WSGI_APPLICATION = 'integration.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
 }
 
 
