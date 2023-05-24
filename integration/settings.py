3ae379d832af7758516d68d84ae9dca30263384c
Cem Gökmen <cem@cemgokmen.com>
2023-05-23 21:52:46 -0700

    Connect to host

--- integration/settings.py
+++ integration/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'postgres',
         'PORT': 5432,
     },
     # 'default': {
