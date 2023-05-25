8f9aeaf29551696e2b04eee67bf8b18ea4a5256e
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2023-05-25 15:48:16 -0700

    Unflip custom synset thing

--- data/management/commands/update.py
+++ data/management/commands/update.py
@@ -79,7 +79,7 @@ class Command(BaseCommand):
             helper function to generate synset hierarchy
             """
             synset_name = synset_sub_hierarchy["name"]
-            synset_is_custom = wn_synset_exists(synset_name)  # TODO: use data from hierarchy. synset_sub_hierarchy["is_custom"] == "1"
+            synset_is_custom = not wn_synset_exists(synset_name)  # TODO: use data from hierarchy. synset_sub_hierarchy["is_custom"] == "1"
             if synset_name != canonicalize(synset_name):
                 print(f"synset {synset_name} is not canonicalized!")
             synset_definition = wn.synset(synset_name).definition() if wn_synset_exists(synset_name) else ""
